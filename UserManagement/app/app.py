e1e5c1045a32a8bea7335ed2d31cdbbf6e040fd0
Ketan6969 <solankiketan156@gmail.com>
2024-12-18 00:09:37 +0530

    added the /profile endpoint to the usermanagement

--- UserManagement/app/app.py
+++ UserManagement/app/app.py
@@ -1,5 +1,6 @@
 import mysql.connector as c,requests
 from flask import Flask, request
+from flask_cors import CORS
 from hashing import hashing
 from db import Db_pooling,db_config
 
@@ -10,7 +11,7 @@ db = Db_pooling(**db_params)
 connection = db.get_connection()
 
 app = Flask(__name__)
-
+CORS(app)
 
 if connection.is_connected:
     print("Connection established ")
@@ -29,7 +30,7 @@ def home():
 def register():
     
     name = request.args.get('name')
-    email = request.args.get('gmail')
+    email = request.args.get('email')
     password = request.args.get('password')
     
     if name and email and password:
@@ -39,7 +40,7 @@ def register():
         with connection.cursor() as cursor:
             cursor.execute(q,(name, email, hashed_password))
             connection.commit()
-            db.release_connection()
+            # db.release_connection(connection)
 
         print(cursor.rowcount, "record inserted.")
         return [{'response' : 'Login Success'}], 201
@@ -75,6 +76,19 @@ def login():
     return [{'response' :  'login success'}], 201
     # return [{'response' : 'Something went wrong!'}]
 
+@app.route("/profile", methods=['GET'])
+def profile():
+    email = request.args.get('email')
+    if email:
+        q = 'select * from test where email = %s'
+        with connection.cursor() as cursor:
+            cursor.execute(q,(email,))
+            data = cursor.fetchone()
+            connection.commit
+            if not data:
+                return[{'response' : 'no user found'}]
+            return [{'response' : data}]
+    return [{'response' : 'please enter an email'}]
     
     
 if __name__ == "__main__":
